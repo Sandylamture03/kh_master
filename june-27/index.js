87baf5652c0966abd37e88fda389da2f3eaab7ea
sandy lamture <sandylamture03@gmail.com>
2022-06-27 23:10:29 +0530

    Javascript code added

---
+++ june-27/index.js
@@ -0,0 +1,26 @@
+let number1 = 10;
+let number2=20;
+let number3=30;
+if(number1>number2&&number1>number3){
+    console.log(number1+"is greatest number");
+} else if(number2>number1&&number2>number3){
+    console.log(number2+"is greatest number");
+} else{
+    console.log(number3+"is greatest number");
+}
+
+
+let Marks=50;
+if(Marks>=80){
+    console.log("Outstanding");
+} else if(Marks>=70){
+    console.log("Honorous");
+} else if(Marks>=60){
+    console.log("First_class");
+} else if(Marks>=50){
+    console.log("Second_class");
+} else if(Marks>=35){
+    console.log("Pass");
+} else{
+    console.log("Fail");
+}
