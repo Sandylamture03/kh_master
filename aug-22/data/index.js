04ec2894b1f165af5f7a840752569539bd607783
sandy lamture <sandylamture03@gmail.com>
2022-08-22 19:57:34 +0530

    till 22-aug

---
+++ aug-22/data/index.js
@@ -0,0 +1,12 @@
+const { application } = require("express");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.json({ fname: "sandeep", lname: "Lamture", age: "25", city: "Pune" });
+});
