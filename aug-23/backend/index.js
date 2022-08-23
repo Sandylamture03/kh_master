69a8e1361258d3f71162e510700347251d5ee48d
sandy lamture <sandylamture03@gmail.com>
2022-08-23 23:52:44 +0530

    aug-23 task

--- aug-23/backend/index.js
+++ aug-23/backend/index.js
@@ -3,7 +3,11 @@ const cors = require("cors");
 const app = express();
 const port = 3000;
 
-app.use(cors());
+const options = {
+  origin: "https://localhost:3001",
+};
+
+app.use(cors(options));
 
 app.listen(port, (_) => {
   console.log("listening on port " + port);
@@ -19,4 +23,32 @@ app.get("/login", (req, res) => {
 
 app.post("/epi1", (req, res) => {
   res.json({ message: "epi1", name: "welcome to post" });
+});
+
+app.get("/add", (req, res) => {
+  console.log(req.query.num1);
+  const num1 = parseInt(req.query.num1);
+  const num2 = parseInt(req.query.num2);
+  res.json({
+    result: num1 + num2,
+  });
+});
+
+app.get("/sub", (req, res) => {
+  // console.log(req.query.num1);
+  const num1 = req.query.num1;
+  const num2 = req.query.num2;
+  res.json({ result: num1 - num2 });
+});
+
+app.get("/mul", (req, res) => {
+  const num1 = parseInt(req.query.num1);
+  const num2 = parseInt(req.query.num2);
+  res.json({ result: num1 * num2 });
+});
+
+app.get("/div", (req, res) => {
+  const num1 = parseInt(req.query.num1);
+  const num2 = parseInt(req.query.num2);
+  res.json({ result: num1 / num2 });
 });
